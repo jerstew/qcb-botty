3ca10b3da127f8529e8a218230bd3fb150d4275b
Jeremy Stewart <jemistmo@gmail.com>
2021-03-21 12:13:57 -0500

    Move arguments model

--- commands/stnk-price.js
+++ commands/stnk-price.js
@@ -2,23 +2,24 @@ const argsParser = require('../utils/args-validate');
 
 module.exports = {
     text: '!stnk-price',
-    args: [
-        {
-            name: 'symbol',
-            required: true
-        },
-        {
-            name: 'watch',
-            required: false
-        }
-    ],
     callback: async (msg) => {
         if (msg) {
-            if (argsParser.validateArguments(msg.content, this.args)) {
+            if (argsParser.validateArguments(msg.content, argumentsModel)) {
                 console.log('proceed with logic yay');
             }
         }
         // var commands = commandParser(msg.content);
         // msg.channel.send("get wrekt");
     }
-  };
+};
+
+const argumentsModel = [
+    {
+        name: 'symbol',
+        required: true
+    },
+    {
+        name: 'watch',
+        required: false
+    }
+];
--- utils/args-validate.js
+++ utils/args-validate.js
@@ -1,14 +1,17 @@
 const dash = require('dashargs');
 var _ = require('lodash/core');
 
-const validateArguments = async (command, argsModel) => {
-    const args = dash.parse(command);
-    console.log(argsModel);
-    if (!checkRequired(args, argsModel)) {
-        msg.channel.send("You are missing required argument(s): " + missing);
-    } else {
-        return;
-    }
+const validateArguments = async (command, argumentsModel) => {
+    const commandArguments = dash.parse(command);
+    let required = [];
+    console.log(argumentsModel.some(item => item.required));
+
+
+    // if (!checkRequired(args, argsModel)) {
+    //     msg.channel.send("You are missing required argument(s): " + missing);
+    // } else {
+    //     return;
+    // }
 };
 
 const checkRequired = async (args, argsModel) => {
